5a5f3bd2b77cce7b8ce7032e22b794aadb9c057e
AidanF86 <aidan.t.foley@gmail.com>
2021-05-18 12:08:48 -0400

    Removed backup files and an unnecessary font

--- archive/actions.h
+++
@@ -1,8 +0,0 @@
-#include "clock.h"
-#include "display.h"
-
-enum modes{textFinished, textRendering, inMenu};
-enum modes mode;
-
-void processKeyDownTextFinished(SDL_Event);
-void processKeyDownTextRendering(SDL_Event);
--- archive/backup/clock.c
+++
@@ -1,9 +0,0 @@
-long double getTotalSeconds()
-{
-    return (long double)currentTime / CLOCKS_PER_SEC;
-}
-
-long double getSecondsSinceLastUpdate()
-{
-    return (long double)(currentTime - lastUpdateTime) / CLOCKS_PER_SEC;
-}
